2f0e8df069a6510638973fa3e63abf06f4ef1a7f
Mariia Rykova <maryrykova24@gmail.com>
2024-04-12 02:04:36 +0400

    feat: add solved task

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -225,8 +225,13 @@ function partialUsingArguments(fn, ...args1) {
  *   getId4() => 7
  *   getId10() => 11
  */
-function getIdGeneratorFunction(/* startFrom */) {
-  throw new Error('Not implemented');
+function getIdGeneratorFunction(startFrom) {
+  let num = startFrom;
+  return function a() {
+    const res = num;
+    num += 1;
+    return res;
+  };
 }
 
 module.exports = {
